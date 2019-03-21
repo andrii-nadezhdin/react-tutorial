92e1f1ae8182c6a3f203644f9da35e5d5db91962
Andrii Nadezhdin <triviy@gmail.com>
2019-03-21 23:14:15 +0200

    Add styles

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
-    headers: { Authorization: 'Client-ID ' }
+    headers: { Authorization: 'Client-ID 85ab605d65a31270f8957000975a45f8d959114e76251da6709ac7a3a37356e4' }
 });
--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './ImageList.css';
 
 const ImageList = (props) => {
     var images = props.images.map(({id, description, urls}) => <img key={id} alt={description} src={urls.regular}></img>);
-    return <div>{images}</div>;
+    return <div className="image-list">{images}</div>;
 }
 
 export default ImageList;
