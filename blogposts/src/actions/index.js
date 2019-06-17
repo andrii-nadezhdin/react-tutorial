f6c69184e9de3722f550df23ea4f8e3da12d2eb1
Andrii Nadezhdin <triviy@gmail.com>
2019-06-17 18:00:40 +0300

    Finalize block posts

---
+++ blogposts/src/actions/index.js
@@ -0,0 +1,18 @@
+import jsonPlaceholder from '../services/jsonPlaceHolder';
+
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+
+    dispatch({
+        type: 'FETCH_POSTS',
+        payload: response.data
+    });
+}
+
+export const fetchUser = userId => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${userId}`);
+    dispatch({
+        type: 'FETCH_USER',
+        payload: response.data
+    });
+}
